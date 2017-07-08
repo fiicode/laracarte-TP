f22ab88c483d0612790722f57096991ce742ef2d
Aboubackr <bahabouackr@yahoo.fr>
2017-07-08 02:30:45 +0000

    Add set_active_rout helpers

--- app/helpers.php
+++ app/helpers.php
@@ -7,9 +7,14 @@ if (!function_exists('page_title')) {
 		if ($title === '') {
 			return $base_title;
 		}else{
-			
+
 		return $title.' | '.$base_title;
 		}
 
+	}
+}
+if (!function_exists('set_active_route')) {
+	function set_active_route($route){
+		return Route::is($route) ? 'active' : '';
 	}
 }
